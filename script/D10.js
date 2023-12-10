2fee54697d6e5af7a2a5a9b5120534c30f86fac4
Alessandro <perrialessandro93@gmail.com>
2023-12-10 19:01:51 +0100

    spero di fare in tempo a finirli, dovevo iniziarli prima

--- script/D10.js
+++ script/D10.js
@@ -96,21 +96,69 @@ console.log("3) DA STRINGA AD ARRAY DI PAROLE:", arrayDaStr);
   Crea una funzione chiamata "deleteOne" che riceve una stringa e un booleano come parametri.
   Se il valore booleano è true la funzione deve ritornare la stringa senza il primo carattere, altrimenti la deve ritornare senza l'ultimo.
 */
-const deleteOne = function () {};
+const deleteOne = function (st, bool) {
+  if (bool === true) {
+    console.log(
+      "4) IL VALORE DI DELETEONE è TRUE, TORNA LA STRINGA SENZA IL PRIMO CARATTERE:",
+      st.slice(1)
+    );
+  } else {
+    console.log(
+      "4) IL VALORE DI DELETEONE è FALSE, TORNA LA STRINGA SENZA L'ULTIMO CARATTERE:",
+      st.slice(0, -1)
+    );
+  }
+};
+deleteOne("yess", false);
 
 /* ESERCIZIO 5
   Crea una funzione chiamata "onlyLetters" che riceve una stringa come parametro e la ritorna eliminando tutte le cifre numeriche.
 
   Es.: onlyLetters("I have 4 dogs") => ritorna "I have dogs"
 */
+const onlyLetters = function (strr) {
+  if (/\d/.test(strr)) {
+    let newSt = strr.replace(/\d/, "");
+    console.log("5) NUMERI ELIMINATI DALLA STRINGA:", newSt);
+  } else {
+    console.log("5) NESSUN NUMERO PRESENTE IN STRINGA:", newSt);
+  }
+};
+onlyLetters("IO HO 3 GECHI");
 
 /* ESERCIZIO 6
   Crea una funzione chiamata "isThisAnEmail" che riceve una stringa come parametro e ritorna true se la stringa è un valido indirizzo email.
 */
+const isThisAnEmail = function (mail) {
+  const email = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (email.test(mail)) {
+    console.log(
+      "6) TRUE, LA STRINGA INSERITA è UN VALIDO INDIRIZZO MAIL:",
+      mail
+    );
+  } else {
+    console.log("6) FALSE, LA STRINGA NON è UN VALIDO INDIRIZZO MAIL:", mail);
+  }
+};
+isThisAnEmail("perrialessandro93@gmail.com");
 
 /* ESERCIZIO 7
   Scrivi una funzione chiamata "whatDayIsIt" che ritorna il giorno della settimana corrente.
 */
+const giorniWeek = [
+  "Domenica",
+  "Lunedì",
+  "Martedì",
+  "Mercoledì",
+  "Giovedì",
+  "Venerdì",
+  "Sabato",
+];
+const whatDayIsIt = function () {
+  const today = new Date().getDay();
+  return giorniWeek[today];
+};
+console.log("7) IL GIORNO ATTUALE è:", whatDayIsIt());
 
 /* ESERCIZIO 8
   Scrivi una funzione chiamata "rollTheDices" che riceve un numero come parametro.
@@ -124,14 +172,55 @@ const deleteOne = function () {};
       values: [3, 3, 4]
   }
 */
+const rollTheDices = function (nEvocazioni) {
+  const estratti = [];
+  let sum = 0;
+  while (estratti.length < nEvocazioni) {
+    const numero = dice();
+    estratti.push(numero);
+    sum += numero;
+  }
+  return { sum, estratti };
+};
+const risultato = rollTheDices(5);
+console.log(
+  "8) I NUMERO ESTRATTI CON DICE SONO:",
+  risultato.estratti,
+  "E LA LORO SOMMA è:",
+  risultato.sum
+);
 
 /* ESERCIZIO 9
   Scrivi una funzione chiamata "howManyDays" che riceve una data come parametro e ritorna il numero di giorni trascorsi da tale data.
 */
+const howManyDays = function (data) {
+  const oggi = new Date();
+  const diffMillisec = Math.abs(oggi - data);
+  const giorniTrascorsi = Math.ceil(diffMillisec / (1000 * 60 * 60 * 24));
+  return giorniTrascorsi;
+};
+const data = new Date("2022-02-11");
+const trascorsi = howManyDays(data);
+console.log("9) NUMERO DI GIORNI TRASCORSI AD OGGI DA", data, ":", trascorsi);
 
 /* ESERCIZIO 10
   Scrivi una funzione chiamata "isTodayMyBirthday" che deve ritornare true se oggi è il tuo compleanno, falso negli altri casi.
 */
+const isTodayMyBirthday = function (birthday) {
+  const oggii = new Date();
+  const compleanno = new Date(birthday);
+  const èIlTuoCompl =
+    oggii.getDate() === compleanno.getDate() &&
+    oggii.getMonth() === compleanno.getMonth();
+  if (èIlTuoCompl) {
+    console.log("10) TANTI AUGURI, OGGI è IL TUO COMPLEANNO:", oggii, true);
+    return true;
+  } else {
+    console.log("10) OGGI NON è IL TUO COMPLEANNO", false);
+    return false;
+  }
+};
+isTodayMyBirthday("2023-12-10");
 
 // Arrays & Oggetti
 
@@ -141,45 +230,140 @@ const deleteOne = function () {};
   Scrivi una funzione chiamata "deleteProp" che riceve un oggetto e una stringa come parametri; deve ritornare l'oggetto fornito dopo aver eliminato
   in esso la proprietà chiamata come la stringa passata come secondo parametro.
 */
+const deleteProp = function (oggetto, str) {
+  const keys = Object.keys(oggetto);
+
+  for (let i = 0; i < keys.length; i++) {
+    const value = oggetto[keys[i]];
+
+    if (typeof value === "string" && value.includes(str)) {
+      oggetto[keys[i]] = value.replace(str, "").trim();
+      console.log(
+        "11) L'OGGETO VIENE RESTITUITO SENZA LA STRINGA FORNITA,",
+        str,
+        ",LA QUALE ERA GIA PRESENTE IN ESSO E VIENE ELIMINATA:",
+        oggetto
+      );
+      return oggetto;
+    }
+  }
+
+  console.log(
+    "11) L'OGGETO VIENE RESTITUITO TALE, LA STRINGA FORNITA NON ERA PRESENTE IN ESSO:",
+    oggetto
+  );
+  return oggetto;
+};
+
+deleteProp({ nomi: "giacomo, andrea, diego" }, "giacomo,");
 
 /* ESERCIZIO 12
   Scrivi una funzione chiamata "newestMovie" che trova il film più recente nell'array "movies" fornito.
 */
+const newestMovie = function () {
+  let i = 0;
+  let ultimo = movies[0];
+  while (i < movies.length) {
+    parseInt(movies[i].Year) > parseInt(ultimo.Year);
+    ultimo = movies[i];
+    i++;
+  }
+  console.log(
+    "12) IL FILM PIù RECENTE NELL'ARRAY MOVIES è: '",
+    ultimo.Title,
+    "'",
+    ", USCITO NELL'ANNO",
+    ultimo.Year
+  );
+};
 
 /* ESERCIZIO 13
   Scrivi una funzione chiamata countMovies che ritorna il numero di film contenuti nell'array "movies" fornito.
 */
+const countMovies = function () {
+  let quantiSono = movies.length;
+  console.log("13) IL NUMERO DI FILM PRESENTI IN ARRAY MOVIES è:", quantiSono);
+};
 
 /* ESERCIZIO 14
   Scrivi una funzione chiamata "onlyTheYears" che crea un array con solamente gli anni di uscita dei film contenuti nell'array "movies" fornito.
 */
+const onlyTheYears = function () {
+  let anni = movies.map((movie) => movie.Year);
+  // anni.push(Years);
+  console.log("14) CREATO ARRAY CONTENENTE SOLO ANNI DI FILM:", anni);
+};
 
 /* ESERCIZIO 15
   Scrivi una funzione chiamata "onlyInLastMillennium" che ritorna solamente i film prodotto nel millennio scorso contenuti nell'array "movies" fornito.
 */
+const onlyInLastMillennium = function () {
+  let scorsoMillennio = movies.filter((movie) => parseInt(movie.Year) < 2000);
+  console.log("15) SOLO FILM PRODOTTI IN MILLENNIO SCORSO:", scorsoMillennio);
+};
 
 /* ESERCIZIO 16
   Scrivi una funzione chiamata "sumAllTheYears" che ritorna la somma di tutti gli anni in cui sono stati prodotti i film contenuti nell'array "movies" fornito.
 */
+const sumAllTheYears = function () {
+  let sommAnni = movies.reduce((acc, film) => acc + parseInt(film.Year), 0);
+  console.log(
+    "16) LA SOMMA DEGLI ANNI CONTENUTA IN ARRAY MOVIES è DI:",
+    sommAnni
+  );
+};
 
 /* ESERCIZIO 17
   Scrivi una funzione chiamata "searchByTitle" che riceve una stringa come parametro e ritorna i film nell'array "movies" fornito che la contengono nel titolo.
 */
+const searchByTitle = function (stringaa) {
+  movies.forEach((titolo) => {
+    if (titolo.Title.includes(stringaa)) {
+      console.log(
+        "17) TRAMITE STRINGA TROVA TITOLO DI FILM DI ARRAY MOVIES:",
+        titolo.Title
+      );
+    }
+  });
+};
 
 /* ESERCIZIO 18
   Scrivi una funzione chiamata "searchAndDivide" che riceve una stringa come parametro e ritorna un oggetto contenente due array: "match" e "unmatch".
   "match" deve includere tutti i film dell'array "movies" fornito che contengono la stringa fornita all'interno del proprio titolo, mentre "unmatch" deve includere tutti i rimanenti.
 */
+const searchAndDivide = function (str) {
+  let match = movies.filter((film) => film.Title.includes(str));
+  console.log(
+    "18) ARRAY MATCH, FILM CONTENENTI STRINGA IN PROPRIO TITOLO:",
+    match
+  );
+  let unmatch = movies.filter((film) => !film.Title.includes(str));
+  console.log(
+    "18) ARRAY UNMATCH, FILM NON CONTENENTI STRINGA IN PROPRIO TITOLO:",
+    unmatch
+  );
+};
 
 /* ESERCIZIO 19
   Scrivi una funzione chiamata "removeIndex" che riceve un numero come parametro e ritorna l'array "movies" fornito privo dell'elemento nella posizione ricevuta come parametro.
 */
+const removeIndex = function (indice) {
+  movies.splice(indice, 1);
+  console.log(
+    "19) ARRAY MOVIES PRIVO DI ELEMENTO IN POSIZIONE RICEVUTA COME PARAMENTRO:",
+    movies
+  );
+};
 
 // DOM (nota: gli elementi che selezionerai non si trovano realmente nella pagina)
 
 /* ESERCIZIO 20
   Scrivi una funzione per selezionare l'elemento dotato di id "container" all'interno della pagina.
 */
+const select = function () {
+  const id = document.getElementById("container");
+  console.log("20) ELEMENTO DOTATO DI ID 'CONTAINER' SELEZIONATO:", id);
+};
 
 /* ESERCIZIO 21
   Scrivi una funzione per selezionare ogni tag <td> all'interno della pagina.
@@ -352,3 +536,30 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
   },
 ];
+
+// 12:
+newestMovie();
+
+// 13:
+countMovies();
+
+// 14:
+onlyTheYears();
+
+// 15:
+onlyInLastMillennium();
+
+// 16:
+sumAllTheYears();
+
+// 17:
+searchByTitle("The Lord");
+
+// 18:
+searchAndDivide("Ave");
+
+// 19:
+removeIndex(10);
+
+// 20:
+select();
